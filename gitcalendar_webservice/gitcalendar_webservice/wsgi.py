fc2f1460b2e0663b31d31276ef55a78a31019929
Benjamin Bauer <benjamin.bauer@dlr.de>
2021-08-10 12:08:32 +0200

    Added django project structure

---
+++ gitcalendar_webservice/gitcalendar_webservice/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gitcalendar_webservice project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gitcalendar_webservice.settings')
+
+application = get_wsgi_application()
