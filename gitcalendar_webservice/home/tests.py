d2877b9685d8994cd89f81f44dc435a536834be8
Benjamin Bauer <benjamin.bauer@dlr.de>
2021-08-10 17:04:55 +0200

    Added initial test framework

--- gitcalendar_webservice/home/tests.py
+++ gitcalendar_webservice/home/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginTests(TestCase):
+
+    def test_not_logged_in(self):
+        """
+        tests whether a user can access the main page
+        """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You are not logged in")
