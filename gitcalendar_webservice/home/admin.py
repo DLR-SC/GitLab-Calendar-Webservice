d57be77ef5210ddf095e73315900b431f60c3d22
Benjamin Bauer <benjamin.bauer@dlr.de>
2021-08-13 11:25:40 +0200

    Add GitLabAPI and CalendarConfiguration models

--- gitcalendar_webservice/home/admin.py
+++ gitcalendar_webservice/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import GitLabAPI, CalendarConfiguration
 
+admin.site.register(GitLabAPI)
+admin.site.register(CalendarConfiguration)
 # Register your models here.
--- gitcalendar_webservice/home/models.py
+++ gitcalendar_webservice/home/models.py
@@ -1,1 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import global_settings
+
+
+class GitLabAPI(models.Model):
+    user_obj = User.get_username()
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE, editable=True)
+    api_name = models.CharField(max_length=100)
+    url = models.URLField()
+    gitlab_api_token = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.api_name
+
+    def __unicode__(self):
+        return self.user.id
+
+
+class CalendarConfiguration(models.Model):
+    config_name = models.CharField(max_length=20)
+    api = models.ForeignKey(GitLabAPI, on_delete=models.CASCADE)
+    only_issues = models.BooleanField(verbose_name="Only Issues")
+    only_milestones = models.BooleanField(verbose_name="Only Milestones")
+
+    def __str__(self):
+        return self.config_name
