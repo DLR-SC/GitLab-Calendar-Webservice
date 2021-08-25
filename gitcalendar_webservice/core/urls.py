e792cdc3df4eb0b01803d555467ae74689739fb5
Benjamin Bauer <benjamin.bauer@dlr.de>
2021-08-25 09:58:01 +0200

    Provided views for CalendarConfiguration

--- gitcalendar_webservice/core/urls.py
+++ gitcalendar_webservice/core/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 from core import views
-from core.views import GitLabAPIListView, AllGitLabAPIListView, GitLabAPIDetailView
+from core.views import GitLabAPIListView, AllGitLabAPIListView, GitLabAPIDetailView, GitLabAPIUpdateView, \
+    GitLabAPICreateView, GitLabAPIDeleteView, CalendarConfigurationListView, AllCalendarConfigurationListView, \
+    CalendarConfigurationDetailView, CalendarConfigurationUpdateView, CalendarConfigurationDeleteView, \
+    CalendarConfigurationCreateView
 
 app_name = 'core'
 
@@ -16,6 +19,13 @@ urlpatterns = [
     path('<int:pk>/gitlabapi/', GitLabAPIListView.as_view(), name='user.gitlabapi.list'),
     path('gitlabapi/', AllGitLabAPIListView.as_view(), name='all.gitlabapi.list'),
     path('gitlabapi/<int:pk>/', login_required(GitLabAPIDetailView.as_view()), name='gitlabapi.detail'),
-    # path('gitlabapi/<int:pk>/edit', login_required(GitLabAPIDetailEditView.as_view()), name='gitlabapi.detail.edit'),
-    path('gitlabapi/add/', login_required(views.index), name='gitlabapi.add'),
+    path('gitlabapi/<int:pk>/edit', login_required(GitLabAPIUpdateView.as_view()), name='gitlabapi.update'),
+    path('gitlabapi/<int:pk>/delete', login_required(GitLabAPIDeleteView.as_view()), name='gitlabapi.delete'),
+    path('gitlabapi/add/', login_required(GitLabAPICreateView.as_view()), name='gitlabapi.add'),
+    path('<int:pk>/calendar/', CalendarConfigurationListView.as_view(), name='user.calendar.list'),
+    path('calendar/', AllCalendarConfigurationListView.as_view(), name='all.calendar.list'),
+    path('calendar/<int:pk>/', login_required(CalendarConfigurationDetailView.as_view()), name='calendar.detail'),
+    path('calendar/<int:pk>/edit', login_required(CalendarConfigurationUpdateView.as_view()), name='calendar.update'),
+    path('calendar/<int:pk>/delete', login_required(CalendarConfigurationDeleteView.as_view()), name='calendar.delete'),
+    path('calendar/add/', login_required(CalendarConfigurationCreateView.as_view()), name='calendar.add'),
 ]
--- gitcalendar_webservice/core/views.py
+++ gitcalendar_webservice/core/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.urls import reverse
@@ -7,30 +8,6 @@ from core.models import GitLabAPI, CalendarConfiguration
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import ListView
 from django.contrib.auth.forms import UserCreationForm
-
-
-class GitLabAPIListView(ListView):
-    model = GitLabAPI
-    template_name = 'gitlab_api_list.html'
-
-    def get_queryset(self):
-        user = get_object_or_404(User, pk=self.kwargs['pk'])
-        return GitLabAPI.objects.filter(user=user)
-
-
-class AllGitLabAPIListView(ListView):
-    model = GitLabAPI
-    template_name = 'gitlab_api_list.html'
-    queryset = GitLabAPI.objects.all()
-
-
-class GitLabAPIDetailView(generic.DetailView):
-    model = GitLabAPI
-    template_name = 'gitlab_api_detail.html'
-
-    def get_queryset(self):
-        api = get_object_or_404(GitLabAPI, pk=self.kwargs['pk'])
-        return GitLabAPI.objects.filter(api_name=api.api_name)
 
 
 def not_found(request, exception):
@@ -50,30 +27,150 @@ def permission_denied(request, exception):
     return HttpResponse("Permission Denied", status=403)
 
 
-def index(request):
-    username = request.user.username
-    api_name = request.POST.get("api_nameInput")
-    url = request.POST.get("urlInput")
-    gitlab_api_token = request.POST.get("gitlab_api_tokenInput")
-    if api_name is None:
-        username = ""
-        api_name = ""
-        url = ""
-        gitlab_api_token = ""
-    context = {
-        'user': username,
-        'api_name': api_name,
-        'url': url,
-        'gitlab_api_token': gitlab_api_token,
-    }
-    try:
-        user_from_database = User.objects.get(username=username)
-        api = GitLabAPI(user=user_from_database, api_name=api_name, url=url, gitlab_api_token=gitlab_api_token)
-        api.save()
-        response = redirect("core:user.gitlabapi.list", user_from_database.id)
-        return response
-    except:
-        return render(request, 'gitlab_api_creation.html', context)
+class GitLabAPIListView(ListView):
+    model = GitLabAPI
+    template_name = 'gitlab_api_list.html'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, pk=self.kwargs['pk'])
+        return GitLabAPI.objects.filter(user=user)
+
+
+class AllGitLabAPIListView(ListView):
+    model = GitLabAPI
+    template_name = 'gitlab_api_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return GitLabAPI.objects.all()
+        else:
+            return GitLabAPI.objects.filter(user=self.request.user)
+
+
+class GitLabAPIDetailView(generic.DetailView):
+    model = GitLabAPI
+    template_name = 'gitlab_api_detail.html'
+
+    def get_queryset(self):
+        api = get_object_or_404(GitLabAPI, pk=self.kwargs['pk'])
+        return GitLabAPI.objects.filter(api_name=api.api_name)
+
+
+class GitLabAPIUpdateView(generic.UpdateView):
+    model = GitLabAPI
+    template_name = 'gitlabapi_form.html'
+    fields = [
+        'api_name', 'url', 'gitlab_api_token'
+    ]
+
+    def get_success_url(self):
+        return reverse('core:user.gitlabapi.list', args=[self.object.user.pk])
+
+
+class GitLabAPICreateView(generic.CreateView):
+    model = GitLabAPI
+    template_name = 'gitlabapi_form.html'
+    fields = [
+        'api_name', 'url', 'gitlab_api_token'
+    ]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('core:gitlabapi.detail', args=[self.object.pk])
+
+
+class GitLabAPIDeleteView(generic.DeleteView):
+    model = GitLabAPI
+    template_name = 'delete.html'
+
+    def get_success_url(self):
+        # decide whether returning to all api page or only to the users himself
+        if self.request.user == self.object.user:
+            return reverse('core:user.gitlabapi.list', args=[self.object.user.pk])
+        else:
+            return reverse('core:all.gitlabapi.list')
+
+
+class CalendarConfigurationListView(ListView):
+    model = CalendarConfiguration
+    template_name = 'calendar_list.html'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, pk=self.kwargs['pk'])
+        return CalendarConfiguration.objects.filter(user=user)
+
+
+class AllCalendarConfigurationListView(ListView):
+    model = CalendarConfiguration
+    template_name = 'calendar_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return CalendarConfiguration.objects.all()
+        else:
+            return CalendarConfiguration.objects.filter(user=self.request.user)
+
+
+class CalendarConfigurationDetailView(generic.DetailView):
+    model = CalendarConfiguration
+    template_name = 'calendar_detail.html'
+
+    def get_queryset(self):
+        cal = get_object_or_404(CalendarConfiguration, pk=self.kwargs['pk'])
+        return CalendarConfiguration.objects.filter(config_name=cal.config_name)
+
+
+class CalendarConfigurationUpdateView(generic.UpdateView):
+    model = CalendarConfiguration
+    template_name = 'calendar_form.html'
+    fields = [
+        'config_name', 'api', 'projects', 'groups', 'only_issues', 'only_milestones', 'combined',
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'].fields['api'].queryset = GitLabAPI.objects.filter(user=self.request.user)
+        return context
+
+    def get_success_url(self):
+        return reverse('core:user.calendar.list', args=[self.object.user.pk])
+
+
+class CalendarConfigurationCreateView(generic.CreateView):
+    model = CalendarConfiguration
+    template_name = 'calendar_form.html'
+    fields = [
+        'config_name', 'api', 'projects', 'groups', 'only_issues', 'only_milestones', 'combined',
+    ]
+
+    # gets the apis which belong to the user
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'].fields['api'].queryset = GitLabAPI.objects.filter(user=self.request.user)
+        return context
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.api.queryset = GitLabAPI.objects.filter(user=self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('core:calendar.detail', args=[self.object.pk])
+
+
+class CalendarConfigurationDeleteView(generic.DeleteView):
+    model = CalendarConfiguration
+    template_name = 'delete.html'
+
+    def get_success_url(self):
+        # decide whether returning to all api page or only to the users himself
+        if self.request.user == self.object.user:
+            return reverse('core:user.calendar.list', args=[self.object.user.pk])
+        else:
+            return reverse('core:all.calendar.list')
 
 
 def signup_view(request):
