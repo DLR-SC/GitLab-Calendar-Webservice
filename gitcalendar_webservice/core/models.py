4c3d0f5f67cb0934ef7a48bd6b8f66de9c884eb6
Benjamin Bauer <benjamin.bauer@dlr.de>
2021-08-28 12:27:39 +0200

    Added relation hints

--- gitcalendar_webservice/core/models.py
+++ gitcalendar_webservice/core/models.py
@@ -1,11 +1,10 @@
 from django.conf import global_settings
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 
 class GitLabAPI(models.Model):
-    user = models.ForeignKey(global_settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(global_settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="api_users")
     api_name = models.CharField(max_length=100)
     url = models.URLField()
     gitlab_api_token = models.CharField(max_length=100)
@@ -13,12 +12,15 @@ class GitLabAPI(models.Model):
     def __str__(self):
         return self.api_name
 
+    def get_related(self):
+        return self.configurations.all()
+
 
 class CalendarConfiguration(models.Model):
-    api = models.ForeignKey(GitLabAPI, on_delete=models.CASCADE)
-    user = models.ForeignKey(global_settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    projects = models.CharField(verbose_name="Project list", max_length=100, default='')
-    groups = models.CharField(verbose_name="Group list", max_length=100, default='')
+    api = models.ForeignKey(GitLabAPI, on_delete=models.CASCADE, related_name="configurations")
+    user = models.ForeignKey(global_settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="cal_users")
+    projects = models.CharField(verbose_name="Project list", max_length=100, default='', blank=True)
+    groups = models.CharField(verbose_name="Group list", max_length=100, default='', blank=True)
     config_name = models.CharField(max_length=100)
     only_issues = models.BooleanField(verbose_name="Only issues", default=False)
     only_milestones = models.BooleanField(verbose_name="Only milestones", default=False)
@@ -27,3 +29,7 @@ class CalendarConfiguration(models.Model):
 
     def __str__(self):
         return self.config_name
+
+    @staticmethod
+    def get_related():
+        return None
