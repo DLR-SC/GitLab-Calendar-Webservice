31787cbf84c3c22ff1ef307e4fc5ee2c74826d31
Benjamin Bauer <benjamin.bauer@dlr.de>
2021-08-28 12:29:02 +0200

    Adapted permission validating

--- gitcalendar_webservice/core/views.py
+++ gitcalendar_webservice/core/views.py
@@ -11,6 +11,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 
+def is_same_user(user1, user2):
+    return user1 == user2
+
+
+def signup_view(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('core:homesite')
+    return render(request, 'registration/signup.html', {'form': form})
+
+
 def not_found(request, exception):
     """
     Shortcut view to render the default 404 site.
@@ -37,15 +53,12 @@ def permission_denied(request, exception):
     return response
 
 
-class GitLabAPIListView(UserPassesTestMixin, ListView):
+class GitLabAPIListView(ListView):
     model = GitLabAPI
     template_name = 'gitlabapi_list.html'
 
-    def test_func(self):
-        return self.request.user.pk == self.kwargs['pk'] or self.request.user.is_superuser
-
     def get_queryset(self):
-        user = get_object_or_404(User, pk=self.kwargs['pk'])
+        user = get_object_or_404(User, pk=self.request.user.pk)
         return GitLabAPI.objects.filter(user=user)
 
 
@@ -54,12 +67,7 @@ class GitLabAPIDetailView(UserPassesTestMixin, generic.DetailView):
     template_name = 'gitlabapi_detail.html'
 
     def test_func(self):
-        return self.request.user == GitLabAPI.objects.get(pk=self.kwargs['pk']).user or \
-             self.request.user.is_superuser
-
-    def get_queryset(self):
-        api = get_object_or_404(GitLabAPI, pk=self.kwargs['pk'])
-        return GitLabAPI.objects.filter(api_name=api.api_name)
+        return is_same_user(self.request.user, self.get_object().user)
 
 
 class GitLabAPIUpdateView(UserPassesTestMixin, generic.UpdateView):
@@ -70,11 +78,10 @@ class GitLabAPIUpdateView(UserPassesTestMixin, generic.UpdateView):
     ]
 
     def test_func(self):
-        return self.request.user == GitLabAPI.objects.get(pk=self.kwargs['pk']).user or \
-             self.request.user.is_superuser
+        return is_same_user(self.request.user, self.get_object().user)
 
     def get_success_url(self):
-        return reverse('core:user.gitlabapi.list', args=[self.object.user.pk])
+        return reverse('core:gitlabapi.detail', args=[self.object.pk])
 
 
 class GitLabAPICreateView(generic.CreateView):
@@ -97,22 +104,18 @@ class GitLabAPIDeleteView(UserPassesTestMixin, generic.DeleteView):
     template_name = 'delete.html'
 
     def test_func(self):
-        return self.request.user == CalendarConfiguration.objects.get(pk=self.kwargs['pk']) \
-               or self.request.user.is_superuser
+        return is_same_user(self.request.user, self.get_object().user)
 
     def get_success_url(self):
-        return reverse('core:user.gitlabapi.list', args=[self.object.user.pk])
+        return reverse('core:gitlabapi.list')
 
 
-class CalendarConfigurationListView(UserPassesTestMixin, ListView):
+class CalendarConfigurationListView(ListView):
     model = CalendarConfiguration
     template_name = 'calendar_list.html'
 
-    def test_func(self):
-        return self.request.user.pk == self.kwargs['pk'] or self.request.user.is_superuser
-
     def get_queryset(self):
-        user = get_object_or_404(User, pk=self.kwargs['pk'])
+        user = get_object_or_404(User, pk=self.request.user.pk)
         return CalendarConfiguration.objects.filter(user=user)
 
 
@@ -121,12 +124,7 @@ class CalendarConfigurationDetailView(UserPassesTestMixin, generic.DetailView):
     template_name = 'calendar_detail.html'
 
     def test_func(self):
-        return self.request.user == CalendarConfiguration.objects.get(pk=self.kwargs['pk']).user \
-               or self.request.user.is_superuser
-
-    def get_queryset(self):
-        cal = get_object_or_404(CalendarConfiguration, pk=self.kwargs['pk'])
-        return CalendarConfiguration.objects.filter(config_name=cal.config_name)
+        return is_same_user(self.request.user, self.get_object().user)
 
 
 class CalendarConfigurationUpdateView(UserPassesTestMixin, generic.UpdateView):
@@ -137,16 +135,15 @@ class CalendarConfigurationUpdateView(UserPassesTestMixin, generic.UpdateView):
     ]
 
     def test_func(self):
-        return self.request.user == CalendarConfiguration.objects.get(pk=self.kwargs['pk']).user \
-               or self.request.user.is_superuser
+        return is_same_user(self.request.user, self.get_object().user)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'].fields['api'].queryset = GitLabAPI.objects.filter(user=self.request.user)
-        return context
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        form.fields['api'].queryset = form.fields['api'].queryset.filter(user=self.request.user)
+        return form
 
     def get_success_url(self):
-        return reverse('core:user.calendar.list', args=[self.object.user.pk])
+        return reverse('core:calendar.detail', args=[self.object.pk])
 
 
 class CalendarConfigurationCreateView(generic.CreateView):
@@ -157,10 +154,10 @@ class CalendarConfigurationCreateView(generic.CreateView):
     ]
 
     # gets the apis which belong to the user
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'].fields['api'].queryset = GitLabAPI.objects.filter(user=self.request.user)
-        return context
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        form.fields['api'].queryset = form.fields['api'].queryset.filter(user=self.request.user)
+        return form
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -175,20 +172,7 @@ class CalendarConfigurationDeleteView(UserPassesTestMixin, generic.DeleteView):
     template_name = 'delete.html'
 
     def test_func(self):
-        return self.request.user == CalendarConfiguration.objects.get(pk=self.kwargs['pk']) \
-               or self.request.user.is_superuser
+        return is_same_user(self.request.user, self.get_object().user)
 
     def get_success_url(self):
-        return reverse('core:user.calendar.list', args=[self.object.user.pk])
-
-
-def signup_view(request):
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect('core:homesite')
-    return render(request, 'registration/signup.html', {'form': form})
+        return reverse('core:calendar.list')
